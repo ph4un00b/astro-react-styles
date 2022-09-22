bdcbe994b9aee1640ce4668dcc4854303a13b995
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2022-09-22 14:30:19 -0500

    1 file modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react(), tailwind(), image()]
+  integrations: [react(), tailwind()],
 });
