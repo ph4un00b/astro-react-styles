89f0d27f66331584aae05656660970e3b9c0a54e
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2022-09-22 14:32:47 -0500

    2 files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import image from '@astrojs/image';
+// import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
