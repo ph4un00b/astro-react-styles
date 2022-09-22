5fc743db29996d60600d1649de074d55605d3524
phau <1057021+ph4un00b@users.noreply.github.com>
2022-09-22 14:45:48 -0500

    chore(Root): upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
-// import image from '@astrojs/image';
-
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true
+  },
   // Enable React to support React JSX components.
   integrations: [react(), tailwind()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,16 @@ module.exports = {
     'node_modules/react-daisyui/dist/**/*.js',
   ],
   plugins: [require('daisyui')],
+  daisyui: {
+   styled: true,
+   themes: ["wireframe"],
+   base: true,
+   utils: false,
+   logs: true,
+   rtl: false,
+   prefix: "",
+   darkTheme: "dark",
+ },
   theme: {
     extend: {},
   },
